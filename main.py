a1c134504de789f67a3459ecda0d7c00f38a687c
MoonEgg <kraiwish123@gmail.com>
2022-09-15 12:47:52 +0700

    seperate function into files

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-def checkBooking(uid, room, date, time, duration):
-    '''
-    check if book-able
-    teacher - 7 days early
-    student - 3 days early
-    '''
-    print("yes")
+import datetime
 
-def addBooking(uid, room, date, time, duration):
-    #book a reservation for this id
-    print("yes")
-
-def printBooking(uid):
-    #display all booking of this id
-    print("yes")
-
-def removeBooking(uid, room, date, time, duration):
-    #cancel a booking
-    print("yes")
+from addBooking import *
+from checkBooking import *
+from printBooking import *
+from removeBooking import *
 
 def main():
     uid = input('please enter your id: ')
