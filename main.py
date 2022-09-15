a3d23488231cea16487f47139e7857478d4b03df
MoonEgg <kraiwish123@gmail.com>
2022-09-15 11:55:01 +0700

    fixed input

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
-# 1 internal file
-# 2 external file
-# 3a Database 
-# 3b online
-#   upload file 
-# 3c interface
-
-''' data type
-id string "08155"
-room
-    library C1, C2, L1 - L4
-    washing machine (9, 4) M1 - M9, W1 - W4 
-    music (8) M1 - M8
-date
-    YYYYMMDD
-time
-    (H, M)
-Duration
-    integer (minutes)
-'''
-
-def checkBooking(id, room, date, time, duration):
+def checkBooking(uid, room, date, time, duration):
     '''
     check if book-able
     teacher - 7 days early
@@ -27,18 +6,17 @@ def checkBooking(id, room, date, time, duration):
     '''
     print("yes")
 
-def addBooking(id, room, date, time, duration):
+def addBooking(uid, room, date, time, duration):
     #book a reservation for this id
     print("yes")
 
-def printBooking(id):
+def printBooking(uid):
     #display all booking of this id
     print("yes")
 
-def removeBooking(id, room, date, time, duration):
+def removeBooking(uid, room, date, time, duration):
     #cancel a booking
     print("yes")
 
-def main():
-    #main interface for program
-    print("Please enter your ID:")
+uid = input('please enter your id: ')
+print(uid)
---
+++ plan.md
@@ -0,0 +1,24 @@
+# PLAN
+
+1. internal file
+2. external file
+3. additional feature
+    1. Database
+    2. online
+        1. upload file
+    3. interface
+
+---
+data type
+
+* id:string-"08155"
+* room
+  * library C1, C2, L1 - L4
+  * washing machine (9, 4) M1 - M9, W1 - W4 
+  * music (8) M1 - M8
+* date
+  * YYYYMMDD
+* time
+  * (H, M)
+* Duration
+  * integer (minutes)
